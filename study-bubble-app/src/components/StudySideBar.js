3aadaef55369dc89b655a908d53438811526fef9
eeshamoona <eeshamoona@gmail.com>
2022-05-15 16:20:45 -0700

    fixed looping

--- study-bubble-app/src/components/StudySideBar.js
+++ study-bubble-app/src/components/StudySideBar.js
@@ -204,7 +204,7 @@ export default function StudySideBar(props) {
         setTaskItems(response);
       });
     }
-  }, [props.refresh, taskItems, props.studyBubble]);
+  }, [props.refresh, props.studyBubble]);
 
   return (
     <Section1>
